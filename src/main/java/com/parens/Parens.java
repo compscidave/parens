96713a304dc63729581cf0ad77eeefba85e47029
compscidave <davidraygodfrey@gmail.com>
2015-02-13 18:14:00 -0600

    add existing project

---
+++ src/main/java/com/parens/Parens.java
@@ -0,0 +1,301 @@
+package com.parens;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static java.lang.Math.*;
+
+
+/**
+   Inputs:  	   Prompts the user for a file which contains any number of
+            	   unparenthesized boolean expressions, each separated by a newline
+	    		   character.  The last expression in the input file MUST NOT be
+	    		   followed by the newline character.
+
+   Functionality:  An expression from the input file shall be read, and the 
+                   number of full parenthesizations of that expression which
+		   		   evaluate to true will be tallied until all expressions
+		   		   from the input file have been processed in this manner.
+
+   Output:  	   A single text file, "output.txt" which contains each original
+            	   ( unparenthesized ) boolean expression from the input file
+	    		   immediately followed by the total number of parenthesizations
+	    		   of that expression which evaluate to true.  The output will have
+	    		   the following form:
+
+	          	   boolean expression : # of true evaluations
+		  		   ...
+**/
+public class Parens {
+
+    private BigInteger Beta[][];
+    private String exp;
+    private int operands = 0;
+	private BigInteger[][] BinCoeff;
+	private long [][] timestamps;
+	private int rows = 50;
+	private boolean calculated = false;
+
+	public static void main(String argsv[]) throws IOException {
+    	
+		String newline = System.getProperty("line.separator"); 
+
+		int operands;
+
+		char [] operators = new char [4];
+		operators[0] = 'a';
+		operators[1] = 'r';
+		operators[2] = 'n';
+		operators[3] = 'x';
+		
+		int[] orands = new int[2];
+		orands[0] = 0;
+		orands[1] = 1;
+		
+	    BufferedWriter out = new BufferedWriter(new FileWriter("src//main//resources//output"));
+	    BufferedWriter inOut = new BufferedWriter(new FileWriter("src//main//resources//input"));
+	    try {
+		    String inputString = "1";
+
+		    for(int i = 1; i <= 100; i++) {
+		    	inputString += "a1";
+		    }
+			inOut.write(inputString);
+		    inOut.close();
+
+		    Parens parens = new Parens(inputString);
+		    operands = ((parens.getExpression().length()-1)/2)+1;
+
+	    	BigInteger truths = parens.calculate();
+
+	    	out.write(parens.getExpression() + ":" + truths + newline);
+	    	//This output creates a tabular view of execution times for each individual
+	    	//entry in the 2d helper matrix. Thus entry 0,operands-1 holds the total time
+	    	//it takes to calculate which parenthesizations evaluate to true over the entire expression.
+			for(int i = 0; i < operands; i++) {
+				out.write(newline);
+				long rowSum = 0;
+				for(int j = 0; j < operands; j++) {
+					out.write("["+parens.getTimestamp(i,j)+"]");
+					if(j != operands - 1) {
+						rowSum += parens.getTimestamp(i,j);
+					} else {
+						out.write("["+rowSum+"]");
+					}
+				}
+			}
+			/*
+			 * 200! / (101! * 100!)
+			 */
+		    out.close();
+		} catch (IOException e) {
+			
+		} finally {
+			inOut.close();
+			out.close();
+		}
+	}
+	
+	public Parens(String exp) throws IllegalArgumentException {
+		if(exp != null && exp.matches("[01]{1}([axrn][01])+")) {
+			this.exp = exp;
+		    operands = ((exp.length()-1)/2)+1;
+		    Beta = new BigInteger[operands][operands];
+			timestamps = new long[operands][operands+1];
+		    DPBinomCoeffs(2*operands,operands);
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	protected BigInteger getTotalTruths() {
+		return Beta[0][operands-1];
+	}
+	
+	public BigInteger calculate() {
+		if(calculated) {
+			return getTotalTruths();
+		} else {
+		    calculateParenthesizations(0,operands-1);
+		    calculated  = true;
+		    return getTotalTruths();
+		}
+	}
+	
+	/**
+    	
+    	 A boolean expression is of the form arg_0 op_0 arg_1 op_1 ... op_n-1 arg_n
+    
+    Inputs:  	   Integers j and k referencing the subexpression arg_j op_j arg_j+1 ... op_k-1 arg_k
+             	   0<=j<=k<=n. Assumes exp.charAt(j) is either 1 or 0 for all j = 2n, 0 <= n <= k and
+             	   exp.charAt(j) is 'a','r','n','x' for all j = 2n + 1; 0 <= n < k.
+    
+    Functionality: Stores the number of full parenthesizations of the expression that evaluate to true
+                   in the public two-dimensional array Beta at position Beta[j][k].  In the initial
+		   		   call to this function j=0, k=n.  i.o.w. the full expression is under consideration
+
+    Properties:    Beta[i][i] = 1 if arg_i is '1' 0 otherwise for all 0<=i<=n
+                   Beta[i][i+1] = 1 if arg_i op_i arg_i+1 is '1', 0 otherwise for all 0<=i<n
+				   Beta[j][k] = Beta[j][j]*Beta[j+1][k] + Beta[j][j+1]*Beta[j+2][k] + ... + Beta[j][k-1]*Beta[j][k]
+		           for all j,k s.t. 0<=j<k-1<=n.
+
+    Output:  	   None. However, each time the function is called, there is one less entry to fill in Beta
+             	   Note that only half of the entries in Beta need to be filled, since Beta[r][t] is undefined for r > t
+		 
+	Exceptions:    Throws OutOfMemoryError during arithmetic operations with -Xmx8g, apparently with operands = 3000 around 48 hours
+				   into execution.
+    **/
+    private void calculateParenthesizations(int j, int k) {
+		//Base Case 1
+		if((k-j) == 0) {
+			//j=k
+		    if(exp.charAt(j*2) == '1')
+		    	Beta[j][k] = BigInteger.ONE;
+		    else
+		    	Beta[j][k] = BigInteger.ZERO;
+    	}
+		//Base Case 2
+		else if((k-j) == 1) {
+		    Beta[j][k] = evaluate(exp.charAt(j*2),exp.charAt(k*2),exp.charAt((j*2)+1));
+		}
+		//Recursive Step
+		else {
+			//This is the entry that is being calculated in this function call
+		    Beta[j][k] = BigInteger.ZERO;
+		    for(int i=j;i<k;i++) {
+				//Break the expression into two parts arg_j ... arg_i and arg_i+1 ... arg_k
+				//If the two entries in Beta haven't been computed, they are computed
+				//by making one recursive call for each entry that hasn't been computed.
+				if(Beta[j][i] == null) {
+					long startMillis = System.currentTimeMillis();
+				    calculateParenthesizations(j,i);
+					timestamps[j][i] = (System.currentTimeMillis() - startMillis);
+				}
+				if(Beta[i+1][k] == null) {
+					long startMillis = System.currentTimeMillis();
+				    calculateParenthesizations(i+1,k);
+					timestamps[i+1][k] = (System.currentTimeMillis() - startMillis);
+				}
+		
+				//this is the operator that connects the two sub-expressions (op_i)
+				char op = exp.charAt((i*2)+1);
+		
+				//Now that Beta[j][i] and Beta[i+1][k] have been calculated
+				//we use them together with the operator connecting them (op_i)
+				//to come closer to a solution for Beta[j][k].
+				if(op == 'a') {
+					// 1 a 1 = 1
+				    Beta[j][k] = Beta[j][k].add(Beta[j][i].multiply(Beta[i+1][k]));
+				} else {
+				    //TOTAL number of full-parenthesizations of the left-hand and right-hand sub-expressions ( respectively )
+				    //note that (jToiParens - Beta[j][i]) = total # of FALSE parenthesizations in the j to i sub-exp.
+				    //likewise, (iAddOneTokParens - Beta[i+1][k]) = # FALSE parens in the i+1 to k sub-exp
+				    BigInteger jToiParens = countParen(i-j);
+				    BigInteger iAddOneTokParens = countParen(k-(i+1));
+				    
+				    if(op == 'r') {
+									// 1 r 1 = 1
+				    	Beta[j][k] = Beta[j][k].add(Beta[j][i].multiply(Beta[i+1][k])
+				    				// 1 r 0 = 1
+				    				.add(Beta[j][i].multiply(iAddOneTokParens.subtract(Beta[i+1][k])))
+				    				// 0 r 1 = 1
+				    				.add((jToiParens.subtract(Beta[j][i])).multiply(Beta[i+1][k])));
+				    } else if(op == 'x') {
+									// 0 x 1 = 1
+				    	Beta[j][k] = Beta[j][k].add((jToiParens.subtract(Beta[j][i])).multiply(Beta[i+1][k]))
+				    				// 1 x 0 = 1
+				    				.add(Beta[j][i].multiply(iAddOneTokParens.subtract(Beta[i+1][k])));
+				    } else if(op == 'n') {
+									// 0 n 1 = 1
+						Beta[j][k] = Beta[j][k].add((jToiParens.subtract(Beta[j][i])).multiply(Beta[i+1][k]))
+									// 1 n 0 = 1
+									.add(Beta[j][i].multiply(iAddOneTokParens.subtract(Beta[i+1][k])))
+									// 0 n 0 = 1
+									.add((jToiParens.subtract(Beta[j][i])).multiply(iAddOneTokParens.subtract(Beta[i+1][k])));
+				    }
+				}
+		    } //end_for
+		}//end recursive step.
+    }
+
+    /**
+       Inputs:  Three characters, l, r, and op representing the
+                left (l) argument, right (r) argument, and the operator
+		(op) that connects the to.
+       
+       Functionality:  evaluates a simple binary expression l op r
+
+       Output:  An integer 1 iff l op r is true, 0 otherwise.
+    **/
+    private static BigInteger evaluate(char l, char r, char op) {
+		if((op == 'a') && (l == '1' && r == '1')) {
+		    return BigInteger.ONE;
+		}
+		if((op =='r') && (l == '1' || r == '1')) {
+		    return BigInteger.ONE;
+		}
+		if((op == 'n') && !(l == '1' && r == '1')) {
+		    return BigInteger.ONE;
+		}
+		if((op == 'x') && !(l == r)) {
+		    return BigInteger.ONE;
+		}
+		return BigInteger.ZERO;
+    }
+
+
+    /**
+       Input:  An integer n representing the number of operators
+               there are in a boolean expression n = { 0, ... , n }
+
+       Functionality:  Using the function DPBinomCoeffs, calculates
+                       how many possible parenthesizations there are
+		       in a boolean expression with n operators
+		       ( n+1 ) operands.
+
+       Output: A long integer of said value.
+    **/
+	private BigInteger countParen(int n) {
+		if (n == 0 || n == 1) {
+			return BigInteger.ONE;
+		} else
+			if(BinCoeff != null) {
+				return BinCoeff[2*n][n].divide(new BigInteger(String.valueOf(n+1)));
+			} else {
+				return DPBinomCoeffs(2 * n, n).divide(
+						new BigInteger(String.valueOf(n + 1)));
+		}
+	}
+
+	/**
+	 * Inputs: Two integers, n and k
+	 * 
+	 * Output: A long integer with value n choose k
+	 **/
+	private BigInteger DPBinomCoeffs(int n, int k) {
+		BinCoeff = new BigInteger[n + 1][k + 1];
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0; j <= min(i, k); j++) {
+				if (j == 0 || j == i)
+					BinCoeff[i][j] = BigInteger.ONE;
+				else
+					BinCoeff[i][j] = BinCoeff[i - 1][j - 1]
+							.add(BinCoeff[i - 1][j]);
+			}
+		}
+		return BinCoeff[n][k];
+	}
+
+	public String getExpression() {
+		return exp;
+	}
+
+	public long getTimestamp(int i, int j) {
+		return timestamps[i][j];
+	}
+
+
+} //end public class Parens
---
+++ src/test/java/com/parens/DynamicParensTest.java
@@ -0,0 +1,295 @@
+package com.parens;
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class DynamicParensTest {
+	
+	private static Parens parens;
+
+	@Test
+	public void testValidExpressionOne() {
+		parens = new Parens("1a1");
+		assertTrue(true);
+	}
+	
+	@Test
+	public void testValidExpressionTwo() {
+		parens = new Parens("1r1x1n0a0");
+		assertTrue(parens.calculate().equals(BigInteger.valueOf(8)));
+		/*
+		 * 1r(1x(1n(0a0))) = 1
+		 * 1r(1x((1n0)a0)) = 1
+		 * 1r((1x1)n(0a0)) = 1
+		 * 1r((1x(1n0))a0) = 1
+		 * 1r(((1x1)n0)a0) = 1
+		 * (1r1)x((1n0)a0) = 1
+		 * (1r(1x1))n(0a0) = 1
+		 * ((1r1)x1)n(0a0) = 1
+		 * 
+		 * All other forms of the expression evaluate to 0 (false). Note: there are six such expressions.
+		 * 
+		 * (1r1)x(1n(0a0)) = 0
+		 * (1r(1x(1n0)))a0 = 0
+		 * (1r((1x1)n0))a0 = 0
+		 * ((1r1)x(1n0))a0 = 0
+		 * ((1r(1x1))n0)a0 = 0
+		 * (((1r1)x1)n0)a0 = 0
+		 */
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression1() {
+		parens = new Parens("1a");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression2() {
+		parens = new Parens("1");
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression3() {
+		parens = new Parens("");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression4() {
+		parens = new Parens("a");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression5() {
+		parens = new Parens("a1");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression6() {
+		parens = new Parens("11");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression7() {
+		parens = new Parens(null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression8() {
+		parens = new Parens("aa");
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpression9() {
+		parens = new Parens("1t1");
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testInvalidExpressionTen() {
+		parens = new Parens("1a2");
+	}
+	
+	@Test
+	public void testOperatorAND() {
+		parens = new Parens("1a1");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("1a0");
+		assertTrue(parens.calculate().equals(BigInteger.ZERO));
+		parens = new Parens("0a1");
+		assertTrue(parens.calculate().equals(BigInteger.ZERO));
+		parens = new Parens("0a0");
+		assertTrue(parens.calculate().equals(BigInteger.ZERO));
+	}
+
+	@Test
+	public void testOperatorOR() {
+		parens = new Parens("1r1");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("1r0");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("0r1");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("0r0");
+		assertTrue(parens.calculate().equals(BigInteger.ZERO));
+	}
+	
+	@Test
+	public void testOperatorXOR() {
+		parens = new Parens("1x1");
+		assertTrue(parens.calculate().equals(BigInteger.ZERO));
+		parens = new Parens("1x0");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("0x1");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("0x0");
+		assertTrue(parens.calculate().equals(BigInteger.ZERO));
+	}
+	
+	@Test
+	public void testOperatorNAND() {
+		parens = new Parens("1n1");
+		assertTrue(parens.calculate().equals(BigInteger.ZERO));
+		parens = new Parens("1n0");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("0n1");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+		parens = new Parens("0n0");
+		assertTrue(parens.calculate().equals(BigInteger.ONE));
+	}
+	
+	@Test
+    public void testCatalanOneHundred() throws IOException {
+		    String inputString = "1";
+		    for(int i = 1; i <= 100; i++) {
+		    	inputString += "a1";
+		    }
+		    parens = new Parens(inputString);
+	    	BigInteger truths = parens.calculate();
+	    	/*
+			 * 200! / (101! * 100!)
+			 */
+			assertTrue(truths.equals(new BigInteger("896519947090131496687170070074100632420837521538745909320")));
+    }
+	
+	/**
+	 * Remove the ignore at your own risk. On a moderately fast quad-core desktop running a 32-bit IDE with maxed ram,
+	 * the following test takes roughly 150-180 seconds to finish. Forcing 64-bit mode through command line or in a 64-bit
+	 * IDE reduces runtime marginally.
+	 * @throws IOException
+	 */
+	@Ignore
+	@Test
+    public void testCatalanOneThousand() throws IOException {
+
+		String inputString = "1";
+		
+		for(int i = 1; i <= 1000; i++) {
+			inputString += "a1";
+		}
+		parens = new Parens(inputString);
+		BigInteger truths = parens.calculate();
+		/*
+		 * the 1000th catalan
+		 * 2000! / (1001! * 1000!)
+		 * 7.25min | ~435sec
+		 */
+		assertTrue(truths.equals(new BigInteger(
+				"2046105521468021692642519982997827217179245642339057975844538099572176010191891863964968026156453752"
+			  + "4490157505694285950973181636343701546373806668828863752033596532433909297174310804435090075047729129"
+			  + "7314225320935212694683984479674769763853760010063791881932656973098208302153805708771117628577790927"
+			  + "5869648636874856805956580057673173655666887003493944650164153396910927037406301799052584663611016897"
+			  + "2728933055321162921432710371407187516258398120726824643431537929562817485824357514814985980875869986"
+			  + "03921577523657477775758899987954012641033870640665444651660246024318184109046864244732001962029120")));
+		
+    }
+
+	@Ignore
+	@Test
+    public void testCatalanOneThousandMixed() throws IOException {
+
+		String inputString = "1";
+		
+		for(int i = 1; i <= 1000; i++) {
+			if(i % 2 == 1) {
+				inputString += "a1";
+			} else {
+				inputString += "n0";
+			}
+		}
+		parens = new Parens(inputString);
+		BigInteger truths = parens.calculate();
+		/*
+		 * roughly 69.46% of the 1000th Catalan.
+		 * (2000! / (1001! * 1000!)) * 0.6946 = ~ the number of true parenthesizations of 1a1n0a1n0a...0a1n0a
+		 */
+		assertTrue(truths.equals(new BigInteger(
+			    "1421185274468560843305167906938290972358115375984645975247433382148829038506213876539251638711215913"
+			  + "9286275039299386596912123497935804162752260437126676755343890699475098983646195612998420718243205797"
+			  + "4587124876423719852510981321872081601010617450661124273163891029547232533041156271349327154365753973"
+			  + "5523867586185986858122272017390327235683873410032462984528545858800306795707109901023343253607697052"
+			  + "9107101826654048708797243040089836615873545129026861646131626199717208279071903700050967517863466394"
+			  + "23119341947429217171822649983119946157251593122932910744565145916236775783333398863354053796342160")));
+			
+		
+    }
+	
+	@Ignore
+	@Test
+    public void testCatalanTwoThousand() throws IOException {
+
+		String inputString = "1";
+		
+		for(int i = 1; i <= 2000; i++) {
+			inputString += "a1";
+		}
+		parens = new Parens(inputString);
+		BigInteger truths = parens.calculate();
+		/*
+		 * 4000! / (2001! * 2000!)
+		 * ~1500000ms | 25min
+		 */
+		assertTrue(truths.equals(new BigInteger(
+				"8310334208065142776309529367618098740710772600642880285497172797668390892100214567591803395202640482"
+			  + "8172947462148184140092293772615069543749919579401913184542190789998131157935053609513254653307083479"
+			  + "0227489302482829125100567918938377462105826094373770187950030383359149306717316457566717966958654007"
+			  + "2380712678409760792703474414242765784871724291694937904834015667075011782054107910351085129485282437"
+			  + "5417656282802051502894290423897489394895646374720720500469015112081379334417342812570507972243484674"
+			  + "0173128707227696585533551197635988680908303537863772991575595714830611217016162275141082663737386119"
+			  + "6388563955715620579274552030664438610113953593262863178924036818352384793166626985408084019051204416"
+			  + "8904920315140097986474066384787791689172949296246077785401782902221715014320983995273869460977894248"
+			  + "5711269847560760415764555065413654067931684397776240990537761972213304506205892470126785741371117041"
+			  + "7196970798565170035697524680103780970820492802926688067380928318938551365171103404275165312933041098"
+			  + "8429748890822794852876294980940940475489863815497115328496368962420559731379861218561596056806164119"
+			  + "183850202316884943615615364399614171566659303464840432312167886200724171888198781484458773618136640")));
+			
+		
+    }
+	
+	@Ignore
+	@Test
+    public void testCatalanTwoThousandFiveHundred() throws IOException {
+
+		String inputString = "1";
+		
+		for(int i = 1; i <= 2500; i++) {
+			inputString += "a1";
+		}
+		parens = new Parens(inputString);
+		BigInteger truths = parens.calculate();
+		/*
+		 * the 2500th Catalan.
+		 * (5000! / (2501! * 2500!))
+		 * 7783162ms | ~2h9m 
+		 */
+		assertTrue(truths.equals(new BigInteger(
+				"6372325811073324173094865225653414983035303615228827423076456972111890196918605232421916521340951948"
+			  + "0876653899462560073247878949956163452771813676347687235870846586258053569529546764678700852556949187"
+			  + "3709975580106879168427203395939388008928916651066837220802520029442006440443458360118666155032486039"
+			  + "3448301921013489332033468612724600544605441130002282307017801209500443011918328243286078726309428680"
+			  + "6437538943056211188415485508070990063347671773915988005312318023950388604433068179084334212486148835"
+			  + "3182598029310714422004124001014749723971907083072078028062436447038158949227251155443023609134022195"
+			  + "3833155143650843562935859361746227885238798166454114251505676480181028528618994725146908028270628152"
+			  + "5196473641203052407592868540468074461542381199319018383974672490708512571989814966610218508975984521"
+			  + "1489587213728693438419567609989641630756456121646480147621855441662438868082646685311287932711068971"
+			  + "2019002683409383781649748979331643248580501339792279862244445524984831061570567360102937264139255313"
+			  + "3050228073973345770891289824902895565462520526296395575911170651046985003611550888482382534586486618"
+			  + "7428554708303266945775772569463194592885574356041992352237762517345945136161929916657407538019415469"
+			  + "1590271023485902900959067805218847887151632693961875387764552894868112253545683751207975128770291419"
+			  + "9653919834809496874969514059034928265648104264306809580718328419418920052250994671632095475125496309"
+			  + "4831183677646245242650458819337600800010903760047699048081828326514477425505277747170597044092916320")));
+			
+		
+    }	
+	
+}
